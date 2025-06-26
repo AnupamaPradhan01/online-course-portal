cbe4ef9f4c1382e1258c468fecf7d3f5adb792ac
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2025-06-26 09:42:08 +0530

    DATABASE CONFIGURATION DONE

---
+++ src/test/java/com/example/online_course_portal/OnlineCoursePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_course_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
