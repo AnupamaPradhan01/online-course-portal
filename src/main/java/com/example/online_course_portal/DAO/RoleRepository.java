0a38a14220d0adfa4e1990612557816493de80d5
AnupamaPradhan01 <anupamakripradhan08@gmail.com>
2025-06-26 10:34:36 +0530

    created dao layer and role repository

---
+++ src/main/java/com/example/online_course_portal/DAO/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.online_course_portal.DAO;
+
+import com.example.online_course_portal.Entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+}
